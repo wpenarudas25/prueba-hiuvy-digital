96296cc47f0e443434632e98fc7a0f16c66c257a
Wilmar Peña Rudas <wpenarudas@gmail.com>
2021-03-07 16:58:45 -0500

    Commit inicial desde eclipse. close #1

---
+++ PruebaHiuvyDigital-1/src/main/java/com/wpenarudas/prueba/PruebaHiuvyDigital1Application.java
@@ -0,0 +1,13 @@
+package com.wpenarudas.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaHiuvyDigital1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaHiuvyDigital1Application.class, args);
+	}
+
+}
